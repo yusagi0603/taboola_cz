b8b03e3350edc22534dcf87dc14f384718321f08
Yu Chen <yu.c@taboola.com>
2025-05-21 16:12:52 +0800

    Update assistant model version to 'gpt-4.1-mini' in config.py

--- exam_maker/config.py
+++ exam_maker/config.py
@@ -5,7 +5,7 @@ CORRECT_PASSWORD = os.getenv('ui_password')
 ASSISTANT_ID = os.getenv('assistant_id')  
 GOOGLE_DRIVE_FOLDER_ID = os.getenv('google_drive_folder_id')
 
-ASSISTANT_MODEL = "gpt-4o"
+ASSISTANT_MODEL = 'gpt-4.1-mini'
 ASSISTANT_NAME = "English reading comprehension passages and question generation"
 ASSISTANT_DESCRIPTION = "Generate an English passage based on the conditions provided by the teacher. Then, create comprehension questions based on the passage and rewrite the questions into different formats."
 VECTOR_STORE_NAME = "english-question"
